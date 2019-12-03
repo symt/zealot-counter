8ef7e21cd0df465d73ef05b4aaa560450534126a
Symt <xxrequired@gmail.com>
2019-12-03 01:21:11 -0600

    version update

--- src/main/java/io/github/symt/ZealotCounter.java
+++ src/main/java/io/github/symt/ZealotCounter.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class ZealotCounter {
 
   static final String MODID = "ZealotCounter";
-  static final String VERSION = "1.0.0";
+  static final String VERSION = "1.0.1";
   private static final String ZEALOT_PATH = "zealotcounter.dat";
   static boolean loggedIn = false;
   static boolean dragonsNest = false;
